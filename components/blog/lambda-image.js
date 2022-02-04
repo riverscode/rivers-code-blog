471c304573cf0ba0ab1017e0a73b423b3412b349
riverscode <rios.renzo@pucp.pe>
2022-02-04 12:14:58 -0500

    Init: Inicio del proyecto en github

--- components/blog/lambda-image.js
+++ components/blog/lambda-image.js
@@ -1,9 +1,9 @@
 import { Box, Image } from "@chakra-ui/react";
 
-export function CustomImage(props){
+export function CustomImage(props) {
   return (
     <Box boxShadow="md" borderWidth="1px" p="6" rounded="md" bg="white">
-      <Image {...props} />
+      <Image {...props} alt="Image" />
     </Box>
   );
-};
+}
