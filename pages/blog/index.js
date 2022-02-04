b204b4989dc8451da2853345e3f6ad9efb7558f9
riverscode <rios.renzo@pucp.pe>
2022-02-04 13:02:11 -0500

    Add: Agregar nuevos post de Lambda

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import Container from "../../components/Container";
 
 import { getFilesMetadata } from "../../lib/mdx";
-import { SearchIcon } from '@chakra-ui/icons'
-import HeaderBlog from '../../components/blog/header-blog';
+import { SearchIcon } from "@chakra-ui/icons";
+import HeaderBlog from "../../components/blog/header-blog";
 import {
   Heading,
   Flex,
@@ -11,61 +11,68 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-} from '@chakra-ui/react'
-import Head from 'next/head';
+} from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Blog({ posts }) {
-  const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState("");
   const filteredBlogPosts = posts
     .sort(
-      (a, b) => Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
+      (a, b) =>
+        Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
     )
     .filter((frontMatter) =>
-      frontMatter.title.toLowerCase().includes(searchValue.toLowerCase()))
+      frontMatter.title.toLowerCase().includes(searchValue.toLowerCase())
+    );
   return (
     <>
-    <Head>
-      <title>River Code | Blog</title>
-    </Head>
-    <Container>
-      <Stack
-        as="main"
-        justifyContent="center"
-        alignItems="flex-start"
-        width="100%"
-        borderBottom={1}
-        >
-        <Flex
-          flexDirection="column"
-          justifyContent="flex-start"
+      <Head>
+        <title>River Code | Blog</title>
+      </Head>
+      <Container>
+        <Stack
+          as="main"
+          justifyContent="center"
           alignItems="flex-start"
           width="100%"
-
+          borderBottom={1}
+        >
+          <Flex
+            flexDirection="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            width="100%"
           >
-          <Heading letterSpacing="tight" mb={4} as="h1" size="2xl">
-            Blog ({posts.length} posts)
-          </Heading>
-          <InputGroup mb={8} w="100%">
-            <Input
-              aria-label="Buscar por título"
-              placeholder="Buscar por título"
-              onChange={(e) => setSearchValue(e.target.value)}
+            <Heading letterSpacing="tight" mb={4} as="h1" size="2xl">
+              Blog ({filteredBlogPosts.length} posts)
+            </Heading>
+            <InputGroup mb={8} w="100%">
+              <Input
+                aria-label="Buscar por título"
+                placeholder="Buscar por título"
+                onChange={(e) => setSearchValue(e.target.value)}
               />
-            <InputRightElement>
-              <SearchIcon color="gray.300" />
-            </InputRightElement>
-          </InputGroup>
-          {!filteredBlogPosts.length && 'No hemos encontrado ningun post 🥺'}
-          {filteredBlogPosts.map((post, index) => {
-            return (
-              <HeaderBlog key={index} title={post.title} publishedAt={post.publishedAt} readingTime={post.readingTime.text} tags={post.tags} slug={post.slug} />
-              )
+              <InputRightElement>
+                <SearchIcon color="gray.300" />
+              </InputRightElement>
+            </InputGroup>
+            {!filteredBlogPosts.length && "No hemos encontrado ningun post 🥺"}
+            {filteredBlogPosts.map((post, index) => {
+              return (
+                <HeaderBlog
+                  key={index}
+                  title={post.title}
+                  publishedAt={post.publishedAt}
+                  readingTime={post.readingTime.text}
+                  tags={post.tags}
+                  slug={post.slug}
+                />
+              );
             })}
-        </Flex>
-      </Stack>
-
-    </Container >
-            </>
+          </Flex>
+        </Stack>
+      </Container>
+    </>
   );
 }
 
